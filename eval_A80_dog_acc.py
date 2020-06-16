44bf704974664d81b97fd81dc95933a3869c59a9
Kenia Chang He <keniachang1997@hotmail.com>
2020-06-16 15:30:25 -0400

    Add and update files to improve and clear out Network A vs Network B experiment

---
+++ eval_A80_dog_acc.py
@@ -0,0 +1,152 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+from mrcnn import utils
+import mrcnn.model as modellib
+import train_coco80_dog_only as train_coco
+from calculate import read_csv
+
+ROOT_DIR = os.path.abspath("./")
+sys.path.append(ROOT_DIR)  # To find local version of the library
+
+config = train_coco.CocoConfig()
+MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+
+# variables
+"""
+    According to class CocoDataset() implementation in train_coco:
+        Annotation dir is ../drive/My Drive/{dataset_folder}/annotations/instances_{dataset_type}{year}.json
+        Images will be loaded via coco_url so no need to worry about the Dataset directory
+"""
+dataset_folder = 'coco_datasets'
+dataset_type = 'val'
+year = '2014'
+
+
+class InferenceConfig(config.__class__):
+    # Run detection on one image at a time
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
+
+def compute_batch_ap(image_ids):
+    APs = []
+    for image_id in image_ids:
+        # Load image
+        image, image_meta, gt_class_id, gt_bbox, gt_mask = modellib.load_image_gt(dataset, config, image_id,
+                                                                                  use_mini_mask=False)
+        # Run object detection
+        results = model.detect([image], verbose=0)
+        # Compute AP
+        r = results[0]
+        AP, precisions, recalls, overlaps = utils.compute_ap(gt_bbox, gt_class_id, gt_mask,
+                                                             r['rois'], r['class_ids'], r['scores'], r['masks'])
+        print(image_id, AP)
+        APs.append(AP)
+    return APs
+
+
+def loop_weight(path_ex_ep, ep):
+    weights_path = path_ex_ep + ("%04d" % ep) + '.h5'
+    print("Loading weights ", weights_path)
+    model.load_weights(weights_path, by_name=True)
+
+    # an option to randomly select 50 image ids for testing
+    image_ids = np.random.choice(dataset.image_ids, 50, replace=False, p=None)
+    APs = compute_batch_ap(image_ids)
+
+    # APs = compute_batch_ap(dataset.image_ids)
+
+    print(APs)
+    print("mAP @ IoU=50: ", np.mean(APs))
+    return np.mean(APs)
+
+
+def save_data(data, path):
+    dataframe = pd.DataFrame(data)
+    dataframe.to_csv(path, index=False, header=False)
+
+
+config = InferenceConfig()
+config.display()
+# with tf.device("/gpu:0"):
+model = modellib.MaskRCNN(mode="inference", config=config, model_dir=MODEL_DIR)
+# Load validation dataset
+dataset = train_coco.CocoDataset()
+dataset.load_coco(dataset_folder, dataset_type, year=year)
+dataset.prepare()
+
+print('\nThe save path for output needs to be the same level as logs folder containing weights.\n')
+# ask users for evaluation mode and the path of accuracy output file
+eval_mode = input('Enter the mode for computing AP (first2/full/last5): ')
+output_path = input('Enter the save path for output '
+                    '(e.g., ../drive/My Drive/mrcnn_coco80_A_weights/dog150_last5_acc.csv): ')
+
+dir_path = os.path.dirname(output_path)
+w_path = dir_path + '/logs/mask_rcnn_coco_'
+
+if eval_mode == 'first2':
+    amount = 2
+    weight_amount = 1 + amount
+    output = np.zeros(amount)
+    i = 0
+    for index in range(1, weight_amount):
+        output[i] = loop_weight(w_path, index)
+        i += 1
+    save_data(output, output_path)
+
+elif eval_mode == 'full':
+    # setup the path of the temp output file which will be saved every 5 times
+    output_dir = os.path.dirname(output_path)
+    filename = (os.path.basename(output_path)).split('.')
+    temp_filename = str(filename[0]) + '_temp.' + str(filename[1])
+    temp_path = os.path.join(output_dir, temp_filename)
+
+    # know when to end
+    m_amount = int(input('Enter the last model number (e.g., 150 for m150):'))
+    end = 1 + m_amount
+
+    # output array
+    output = np.zeros(m_amount)
+
+    # ask if user want to resume computation of mAPs
+    is_resumed = input('Do you want to resume computations of mAPs from before? (y/n): ')
+    if is_resumed.lower() == 'y':
+        mAPs_file = input('Enter the previous output file path: ')
+        start = int(input('Start from which for mAPs computation (e.g., 20 mAPs(0-19) saved, then 21)? Enter: '))
+        previous_mAPs = np.asarray(read_csv(mAPs_file), dtype=np.float64)
+        current_shape = start - 1
+        previous_mAPs = previous_mAPs[:current_shape]
+        previous_mAPs = previous_mAPs.reshape(current_shape)
+        output[:current_shape] = previous_mAPs
+    else:
+        start = 1
+
+    i = start - 1
+    for index in range(start, end):
+        output[i] = loop_weight(w_path, index)
+        i += 1
+
+        if (index % 5 == 0) and (index + 1 != end):
+            save_data(output, temp_path)
+
+    save_data(output, output_path)
+    os.remove(temp_path)
+
+elif eval_mode == 'last5':
+    m_num = int(input('Enter the last model number (e.g., 150 for m150):'))
+    weight_amount = 1 + m_num
+    amount = 5
+    start_num = weight_amount - amount
+    output = np.zeros(amount)
+    i = 0
+    for index in range(start_num, weight_amount):     # 146-151 = m146 to m150
+        output[i] = loop_weight(w_path, index)
+        i += 1
+    save_data(output, output_path)
+
+else:
+    print('Invalid mode entered!')
+
+print('Finish process.')
--- extend_network_to_array.py
+++ extend_network_to_array.py
@@ -3,22 +3,18 @@ import sys
 import numpy as np
 import mrcnn.model as modellib
 import train_coco1_dog_only as train_coco1_dog
-import train_coco80_all_dog as train_coco80_all
+import train_coco80_all as train_coco80_all
+import train_coco80_dog_only as train_coco80_dog
 
 ROOT_DIR = os.path.abspath("./")
 sys.path.append(ROOT_DIR)  # To find local version of the library
 
 coco_config1 = train_coco1_dog.CocoConfig()
-coco_config80 = train_coco80_all.CocoConfig()
+coco_config80B = train_coco80_all.CocoConfig()
+coco_config80A = train_coco80_dog.CocoConfig()
 MODEL_DIR1 = os.path.join(ROOT_DIR, "logs1")
-MODEL_DIR80 = os.path.join(ROOT_DIR, "logs80")
-
-# variables
-print('Enter the weight file path of the specified coco model accordingly.')
-print('Example 1, ../drive/My Drive/mrcnn_coco_A_weights/logs/mask_rcnn_coco_0001.h5')
-print('Example 2, ../drive/My Drive/mrcnn_coco_B_weights/logs/mask_rcnn_coco_0001.h5')
-weight1_path = input('The model with config.NUM_CLASSES=1+1 which train and infer on only dog data: ')
-weight80_path = input('The model with config.NUM_CLASSES=1+80 which train and infer on all data: ')
+MODEL_DIR80B = os.path.join(ROOT_DIR, "logs80")
+MODEL_DIR80A = os.path.join(ROOT_DIR, "Alogs80")
 
 
 class InferenceConfig1(coco_config1.__class__):
@@ -27,7 +23,13 @@ class InferenceConfig1(coco_config1.__class__):
     IMAGES_PER_GPU = 1
 
 
-class InferenceConfig80(coco_config80.__class__):
+class InferenceConfig80B(coco_config80B.__class__):
+    # Run detection on one image at a time
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
+
+class InferenceConfig80A(coco_config80A.__class__):
     # Run detection on one image at a time
     GPU_COUNT = 1
     IMAGES_PER_GPU = 1
@@ -57,24 +59,43 @@ def extend_to_array(weights):
 
 
 if __name__ == '__main__':
-    config1 = InferenceConfig1()
-    config80 = InferenceConfig80()
+    networks = []
+    while True:
+        network = input('Enter the name of network if you want to extend it (coco1/coco80B/coco80A/n)')
+        if network == 'coco1' or network == 'coco80B' or network == 'coco80A':
+            networks.append(network)
+            networks = list(set(networks))
+            if len(networks) == 3:
+                break
+        else:
+            break
 
-    model1 = modellib.MaskRCNN(mode="inference", config=config1, model_dir=MODEL_DIR1)
-    model80 = modellib.MaskRCNN(mode="inference", config=config80, model_dir=MODEL_DIR80)
+    flatten_networks = {}
+    print('Enter the weight file path of the specified coco model accordingly.')
+    print('For example, ../drive/My Drive/A1vsB80/logs1/mask_rcnn_coco_0001.h5')
+    for network in networks:
+        if network == 'coco1':
+            weight_path = input('The model with config.NUM_CLASSES=1+1 which train and infer on only dog data: ')
+            config = InferenceConfig1()
+            model_dir = MODEL_DIR1
+        elif network == 'coco80B':
+            weight_path = input('The model with config.NUM_CLASSES=1+80 which train and infer on all data: ')
+            config = InferenceConfig80B()
+            model_dir = MODEL_DIR80B
+        else:
+            weight_path = input('The model with config.NUM_CLASSES=1+80 which train and infer on only dog data: ')
+            config = InferenceConfig80A()
+            model_dir = MODEL_DIR80A
 
-    model1.load_weights(weight1_path, by_name=True)
-    model80.load_weights(weight80_path, by_name=True)
+        model = modellib.MaskRCNN(mode="inference", config=config, model_dir=model_dir)
+        model.load_weights(weight_path, by_name=True)
+        model_weights = model.get_weights()
+        model_network_flatten_array = extend_to_array(model_weights)
+        # print(len(model_network_flatten_array))
+        flatten_networks[network] = len(model_network_flatten_array)
 
-    # get the weights array of each model
-    model1_weights = model1.get_weights()
-    model80_weights = model80.get_weights()
-
-    model1_network_flatten_array = extend_to_array(model1_weights)
-    model80_network_flatten_array = extend_to_array(model80_weights)
-
-    # compare shape/size of the two networks?
-    print(len(model1_network_flatten_array))
-    print(len(model80_network_flatten_array))
+    # In summary, print each network name and its size
+    for name, size in flatten_networks.items():
+        print((name + ':'), size)
 
     print('Finish process.')
