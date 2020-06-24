7f84fa6d8a848791b7f38165e2d34675f4de8f7a
Kenia Chang <keniachang1997@gmail.com>
2020-06-24 16:01:19 -0400

    Prepare for training networks

--- dl_coco.py
+++ dl_coco.py
@@ -1,6 +1,7 @@
 import os
 from PythonAPI.pycocotools.coco import COCO
 import requests
+from find_catID import coco_categories
 from extract_category_imgIds import get_specified_category_image_ids
 import skimage.io
 import matplotlib.pyplot as plt
@@ -8,80 +9,106 @@ import matplotlib.pyplot as plt
 
 # https://github.com/cocodataset/cocoapi/issues/271
 
-# # download the images of a specific category of the coco dataset
-# dataset = input('Enter the dataset to be downloaded (train2017/val2017): ')
-# category = input('Enter the category to be downloaded (dog, cake, bed): ')
-#
-# data_folder_path = './cocoDS/' + dataset + '/'
-# os.makedirs(data_folder_path, exist_ok=True)
-#
-# ann_file = './cocoDS/annotations/instances_' + dataset + '.json'
-# coco = COCO(ann_file)
-# # cats = coco.loadCats(coco.getCatIds())
-# # nms = [cat['name'] for cat in cats]
-# # print('COCO categories: \n{}\n'.format(' '.join(nms)))
-#
-# # trainval2017 - 'dog' (4562), 'cake' (3049), 'bed' (3831)
-# # 'dog' - train=4385 & test=177
-# # 'cake' - train=2925?/2859? & test=124?/123?   (# img downloaded)?/(# in folder)?
-# # 'bed' - train=3682?/3319? & test=149?/131?
-# # totoal train of 3 categories = 4385+2859+3319=10563
-# # totoal val of 3 categories = 4385+2859+3319=431
-# # 11442 - 10994 = 448 images are absent
-# catIds = coco.getCatIds(catNms=[category])
-# imgIds = coco.getImgIds(catIds=catIds)
-# images = coco.loadImgs(imgIds)
-# # print("imgIds: ", imgIds)
-# # print("images: ", images)
-#
-# i = 0
-# for im in images:
-#     # print("im: ", im)
-#
-#     img_data = requests.get(im['coco_url']).content
-#
-#     with open(data_folder_path + im['file_name'], 'wb') as handler:
-#         handler.write(img_data)
-#
-#     i += 1
-#     print('Images Downloaded: ' + str(i))
+if __name__ == '__main__':
+    # # download the images of a specific category of the coco dataset
+    # dataset = input('Enter the dataset to be downloaded (train2017/val2017): ')
+    # category = input('Enter the category to be downloaded (dog, cake, bed): ')
+    #
+    # data_folder_path = './cocoDS/' + dataset + '/'
+    # os.makedirs(data_folder_path, exist_ok=True)
+    #
+    # ann_file = './cocoDS/annotations/instances_' + dataset + '.json'
+    # coco = COCO(ann_file)
+    # # cats = coco.loadCats(coco.getCatIds())
+    # # nms = [cat['name'] for cat in cats]
+    # # print('COCO categories: \n{}\n'.format(' '.join(nms)))
+    #
+    # # trainval2017 - 'dog' (4562), 'cake' (3049), 'bed' (3831)
+    # # 'dog' - train=4385 & test=177
+    # # 'cake' - train=2925?/2859? & test=124?/123?   (# img downloaded)?/(# in folder)?
+    # # 'bed' - train=3682?/3319? & test=149?/131?
+    # # totoal train of 3 categories = 4385+2859+3319=10563
+    # # totoal val of 3 categories = 4385+2859+3319=431
+    # # 11442 - 10994 = 448 images are absent
+    # catIds = coco.getCatIds(catNms=[category])
+    # imgIds = coco.getImgIds(catIds=catIds)
+    # images = coco.loadImgs(imgIds)
+    # # print("imgIds: ", imgIds)
+    # # print("images: ", images)
+    #
+    # i = 0
+    # for im in images:
+    #     # print("im: ", im)
+    #
+    #     img_data = requests.get(im['coco_url']).content
+    #
+    #     with open(data_folder_path + im['file_name'], 'wb') as handler:
+    #         handler.write(img_data)
+    #
+    #     i += 1
+    #     print('Images Downloaded: ' + str(i))
 
-# download pure dog imgaes from val2014 for Networks A, B, C for evaluation
-# save_data_dir = './coco_val2014_pure_dog_images/'
-coco = COCO('./cocoDS/annotations/instances_val2014.json')
-selected_cat_id0 = 18   # dog
-image_ids = get_specified_category_image_ids(coco, selected_cat_id0)
-print(image_ids)
+    # # download pure dog imgaes from val2014 for Networks A, B, C for evaluation
+    # # save_data_dir = './coco_val2014_pure_dog_images/'
+    # coco = COCO('./cocoDS/annotations/instances_train2014.json')
+    # print()
+    #
+    # # download images
+    # selected_cat_id0 = 18   # dog
+    # image_ids = get_specified_category_image_ids(coco, selected_cat_id0)
+    # print(image_ids)
+    # i = 0
+    # for img_id in image_ids:
+    #
+    #     img_data = requests.get(coco.imgs[img_id]['coco_url']).content
+    #
+    #     with open(save_data_dir + coco.imgs[img_id]['file_name'], 'wb') as handler:
+    #         handler.write(img_data)
+    #
+    #     i += 1
+    #     print('Images Downloaded: ' + str(i))
+    #
+    # print('\nFinish downloading pure dog images.')
 
-# # download images
-# i = 0
-# for img_id in image_ids:
-#
-#     img_data = requests.get(coco.imgs[img_id]['coco_url']).content
-#
-#     with open(save_data_dir + coco.imgs[img_id]['file_name'], 'wb') as handler:
-#         handler.write(img_data)
-#
-#     i += 1
-#     print('Images Downloaded: ' + str(i))
-#
-# print('\nFinish downloading pure dog images.')
+    # # test to access the pure dog data
+    # image_dir = './coco_val2014_pure_dog_images'
+    # for i in image_ids:
+    #     path = os.path.join(image_dir, coco.imgs[i]['file_name'])
+    #     try:
+    #         image = skimage.io.imread(path)
+    #     except:
+    #         image = []
+    #         print('Do not use this image and its annotation for training/testing!')
+    #     # print(image)
+    #
+    #     print('image id:', i)
+    #     print('filename:', coco.imgs[i]['file_name'])
+    #
+    #     # Draw image
+    #     plt.axis('off')
+    #     plt.imshow(image)
+    #     plt.show()
 
-# # test to access the pure dog data
-# image_dir = './coco_val2014_pure_dog_images'
-# for i in image_ids:
-#     path = os.path.join(image_dir, coco.imgs[i]['file_name'])
-#     try:
-#         image = skimage.io.imread(path)
-#     except:
-#         image = []
-#         print('Do not use this image and its annotation for training/testing!')
-#     # print(image)
-#
-#     print('image id:', i)
-#     print('filename:', coco.imgs[i]['file_name'])
-#
-#     # Draw image
-#     plt.axis('off')
-#     plt.imshow(image)
-#     plt.show()
+    # size of each label pure data in train2014
+    coco = COCO('./cocoDS/annotations/instances_train2014.json')
+    print()
+    qualified_size = 200
+    qualified_labels = []
+    minimum_size = qualified_size
+    label = ''
+    for category in coco_categories:
+        class_id = (coco.getCatIds(catNms=[category]))[0]
+
+        image_ids = get_specified_category_image_ids(coco, class_id)
+        subset_size = len(image_ids)
+        print(category, subset_size, '\n')
+
+        if subset_size < minimum_size:
+            minimum_size = subset_size
+            label = category
+        elif subset_size >= qualified_size:
+            qualified_labels.append(category)
+
+    print('Amount of label with qualified size:', len(qualified_labels))
+    print('Qualified labels:', qualified_labels)
+    print('Smallest:', label, '=', minimum_size)
--- find_catID.py
+++ find_catID.py
@@ -1,57 +1,92 @@
 from PythonAPI.pycocotools.coco import COCO
 # import json
 
-# coco categories names
+# coco 80 categories names
 coco_categories = ['person', 'bicycle', 'car', 'motorcycle', 'airplane',
                    'bus', 'train', 'truck', 'boat', 'traffic light',
                    'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
                    'cat', 'dog', 'horse', 'sheep', 'cow',
                    'elephant', 'bear', 'zebra', 'giraffe', 'backpack',
                    'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee',
-                   'skis', 'snowboard', 'sports', 'ball', 'kite',
-                   'baseball', 'bat', 'baseball glove', 'skateboard', 'surfboard',
-                   'tennis racket', 'bottle', 'wine glass', 'cup', 'fork',
-                   'knife', 'spoon', 'bowl', 'banana', 'apple',
-                   'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog',
-                   'pizza', 'donut', 'cake', 'chair', 'couch',
-                   'potted plant', 'bed', 'dining table', 'toilet', 'tv',
-                   'laptop', 'mouse', 'remote keyboard', 'cell phone', 'microwave oven',
+                   'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat',
+                   'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle',
+                   'wine glass', 'cup', 'fork', 'knife', 'spoon',
+                   'bowl', 'banana', 'apple', 'sandwich', 'orange',
+                   'broccoli', 'carrot', 'hot dog', 'pizza', 'donut',
+                   'cake', 'chair', 'couch', 'potted plant', 'bed',
+                   'dining table', 'toilet', 'tv', 'laptop', 'mouse',
+                   'remote', 'keyboard', 'cell phone', 'microwave', 'oven',
                    'toaster', 'sink', 'refrigerator', 'book', 'clock',
                    'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
 
-dataset_dir = './cocoDS'
-year = '2017'
-dataset = input('Enter the dataset (train2017/val2017): ')
-categories = []  # ['dog', 'cake', 'bed']
-while True:
-    category = input('Enter the category name (dog/cake/bed), or no to stop: ')
-    if category in coco_categories:
-        categories.append(category)
-    elif category == 'no':
-        break
-    else:
-        print('Unknown category has been entered!')
+if __name__ == '__main__':
+    # # get image_ids of subset
+    # dataset_dir = './cocoDS'
+    # year = '2017'
+    # dataset = input('Enter the dataset (train2017/val2017): ')
+    # categories = []  # ['dog', 'cake', 'bed']
+    # while True:
+    #     category = input('Enter the category name (dog/cake/bed), or no to stop: ')
+    #     if category in coco_categories:
+    #         categories.append(category)
+    #     elif category == 'no':
+    #         break
+    #     else:
+    #         print('Unknown category has been entered!')
+    #
+    # # data_folder_path = './cocoDS/annotations_subset/'
+    # # os.makedirs(data_folder_path, exist_ok=True)
+    #
+    # if dataset == 'train2017':
+    #     subset = 'train'
+    # elif dataset == 'val2017':
+    #     subset = 'val'
+    # else:
+    #     print('Invalid dataset was entered!')
+    #     exit(-1)
+    #
+    # coco = COCO("{}/annotations/instances_{}{}.json".format(dataset_dir, subset, year))
+    #
+    # class_ids = []
+    # for category in categories:
+    #     class_ids.extend(list(coco.getCatIds(catNms=[category])))
+    # print(categories, class_ids)
+    #
+    # # image_ids = []
+    # # for class_id in class_ids:
+    # #     image_ids.extend(list(coco.getImgIds(catIds=[class_id])))
+    # # # Remove duplicates
+    # # image_ids = list(set(image_ids))
 
-# data_folder_path = './cocoDS/annotations_subset/'
-# os.makedirs(data_folder_path, exist_ok=True)
+    # calculate subset size
+    categories = ['car', 'dog', 'cake']
+    """
+    car: 8606
+    Currently: 8606
+    
+    dog: 3041
+    Currently: 11323
+    
+    cake: 2080
+    Currently: 13328
+    """
+    coco = COCO("./cocoDS/annotations/instances_train2014.json")
+    print()
+    all_image_ids = []
+    for category in categories:
+        class_ids = coco.getCatIds(catNms=[category])
 
-if dataset == 'train2017':
-    subset = 'train'
-elif dataset == 'val2017':
-    subset = 'val'
-else:
-    print('Invalid dataset was entered!')
-    exit(-1)
+        image_ids = []
+        for cat_id in class_ids:
+            image_ids.extend(list(coco.getImgIds(catIds=[cat_id])))
+        # Remove duplicates
+        image_ids = list(set(image_ids))
 
-coco = COCO("{}/annotations/instances_{}{}.json".format(dataset_dir, subset, year))
+        all_image_ids.extend(image_ids)
+        all_image_ids = list(set(all_image_ids))
 
-class_ids = []
-for category in categories:
-    class_ids.extend(list(coco.getCatIds(catNms=[category])))
-print(categories, class_ids)
+        print(category + ':', len(image_ids))
+        print('Currently:', len(all_image_ids))
+        print()
 
-# image_ids = []
-# for class_id in class_ids:
-#     image_ids.extend(list(coco.getImgIds(catIds=[class_id])))
-# # Remove duplicates
-# image_ids = list(set(image_ids))
+    print('Finish process.')
