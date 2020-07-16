909bb90a47d70e1a46cc8939f9f759f9534593c6
Kenia Chang <keniachang1997@gmail.com>
2020-07-15 20:09:25 -0400

    Train a mrcnn model on real world worker and one on silhouette worker

--- worker_train.py
+++ worker_train.py
@@ -5,51 +5,44 @@ Copyright (c) 2018 Matterport, Inc.
 Licensed under the MIT License (see LICENSE for details)
 Written by Waleed Abdulla
 ------------------------------------------------------------
-Usage: import the module (see Jupyter notebooks for examples), or run from
-       the command line as such:
-    # Train a new model starting from pre-trained COCO weights
-    python3 worker.py train --dataset=/path/to/workers/dataset --weights=coco
-    # Resume training a model that you had trained earlier
-    python3 worker.py train --dataset=/path/to/workers/dataset --weights=last
-    # Train a new model starting from ImageNet weights
-    python3 worker.py train --dataset=/path/to/workers/dataset --weights=imagenet
-    # Apply color splash to an image
-    python3 worker.py splash --weights=/path/to/weights/file.h5 --image=<URL or path to file>
-    # Apply color splash to video using the last weights you trained
-    python3 worker.py splash --weights=last --video=<URL or path to file>
 """
-# test
+
 import os
+import sys
 import json
-import datetime
 import numpy as np
 import skimage.io
 import skimage.draw
+import argparse
+import math
 
 # Root directory of the project
 ROOT_DIR = os.path.abspath("./")
-
-# Import Mask RCNN
+sys.path.append(ROOT_DIR)  # To find local version of the library
 from mrcnn.config import Config
 from mrcnn import model as modellib, utils
 
 # Path to trained weights file
-COCO_WEIGHTS_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+# COCO_WEIGHTS_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+initial_weight_path = '../drive/My Drive/NetwB_InitW/mrcnn_coco_0001.h5'
 
-
-# Directory to save logs and model checkpoints, if not provided
-# through the command line argument --logs
-# DEFAULT_LOGS_DIR = os.path.join(ROOT_DIR, "logs")
-
-############################################################
-#  Configurations
-############################################################
+# Parse command line arguments
+parser = argparse.ArgumentParser(description='Train Mask R-CNN on worker data.')
+parser.add_argument('--dataset', required=True,
+                    metavar="<WorkerData|silhouette320_latest>",
+                    help='Directory of the MS-COCO dataset')
+parser.add_argument('--logs', required=True,
+                    metavar="<mrcnn_worker_real|mrcnn_worker_silhouette>",
+                    help='Logs and checkpoints directory (../drive/My Drive/{}/logs)')
+args = parser.parse_args()
+dataset = args.dataset
+assert dataset == 'WorkerData' or dataset == 'silhouette320_latest'
+log_folder = args.logs
+print("Dataset: ", args.dataset)
+print("Logs: ", args.logs)
 
 
 class WorkerConfig(Config):
-    """Configuration for training on the action dataset.
-    Derives from the base Config class and overrides some values.
-    """
     # Give the configuration a recognizable name
     NAME = "worker"
 
@@ -61,31 +54,21 @@ class WorkerConfig(Config):
     NUM_CLASSES = 1 + 4  # Background + action
 
     # Number of training steps per epoch
-    STEPS_PER_EPOCH = 120
+    STEPS_PER_EPOCH = 10
 
     # Number of validation steps to run at the end of every training epoch.
     # A bigger number improves accuracy of validation stats, but slows
     # down the training.
-    # VALIDATION_STEPS = 60
-
-    # Length of square anchor side in pixels
-    # RPN_ANCHOR_SCALES = (32, 64, 128, 256, 512)
-    # RPN_A#NCHOR_SCALE = (4, 8, 16, 32, 64)
-    # RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)
+    VALIDATION_STEPS = 20
 
     # Skip detections with < 80% confidence
     DETECTION_MIN_CONFIDENCE = 0.8
-    DEFAULT_LOGS_DIR = '../drive/My Drive/silhouette_weight/logs'
-    # TRAIN_ROIS_PER_IMAGE = 512
 
-
-############################################################
-#  Dataset
-############################################################
+    DEFAULT_LOGS_DIR = '../drive/My Drive/{}/logs'.format(log_folder)
 
 
 class WorkerDataset(utils.Dataset):
-    def load_worker(self, dataset_dir, subset):
+    def load_worker(self, dataset_dir, subset, return_subset_size=False):
         """Load a subset of the worker dataset.
         dataset_dir: Root directory of the dataset.
         subset: Subset to load: train or val
@@ -122,6 +105,8 @@ class WorkerDataset(utils.Dataset):
         # annotations. Skip unannotated images.
         annotations = [a for a in annotations if a['regions']]
 
+        print(subset, 'size:', len(annotations))
+
         # Add images
         for a in annotations:
             # Get the x, y coordinates of points of the polygons that make up
@@ -139,7 +124,7 @@ class WorkerDataset(utils.Dataset):
             image_path = os.path.join(dataset_dir, a['filename'])
             image = skimage.io.imread(image_path)
             height, width = image.shape[:2]
-            print(height, width)
+            # print(height, width)
 
             self.add_image(
                 "action",
@@ -148,6 +133,9 @@ class WorkerDataset(utils.Dataset):
                 class_id=name_id,
                 width=width, height=height,
                 polygons=polygons)
+
+        if return_subset_size:
+            return len(annotations)
 
     def load_mask(self, image_id):
         """Generate instance masks for an image.
@@ -179,8 +167,11 @@ class WorkerDataset(utils.Dataset):
             # print("rr.shape, min(rr),max(rr): {}, {},{}".format(rr.shape, np.min(rr), np.max(rr)))
             # print("cc.shape, min(cc),max(cc): {}, {},{}".format(cc.shape, np.min(cc), np.max(cc)))
 
-            ## Note that this modifies the existing array arr, instead of creating a result array
-            ## Ref: https://stackoverflow.com/questions/19666626/replace-all-elements-of-python-numpy-array-that-are-greater-than-some-value
+            """
+            Note that this modifies the existing array arr, instead of creating a result array
+            Ref: https://stackoverflow.com/questions/19666626/replace-all-elements-of-python-numpy-array-that-are-
+            greater-than-some-value
+            """
             rr[rr > mask.shape[0] - 1] = mask.shape[0] - 1
             cc[cc > mask.shape[1] - 1] = mask.shape[1] - 1
 
@@ -189,8 +180,11 @@ class WorkerDataset(utils.Dataset):
             # print("cc.shape, min(cc),max(cc): {}, {},{}".format(cc.shape, np.min(cc), np.max(cc)))
 
             mask[rr, cc, i] = 1
-            # Return mask, and array of class IDs of each instance. Since we have
-        # one class ID only, we return an array of 1s
+
+            """
+            Return mask, and array of class IDs of each instance. Since we have one class ID only, we return an array 
+            of 1s
+            """
         # return (mask.astype(np.bool), class_ids)
         return mask.astype(np.bool), class_ids
 
@@ -207,7 +201,7 @@ def train(model, dataset):
     """Train the model."""
     # Training dataset.
     dataset_train = WorkerDataset()
-    dataset_train.load_worker(dataset, "train")
+    train_subset_size = dataset_train.load_worker(dataset, "train", return_subset_size=True)
     dataset_train.prepare()
 
     # Validation dataset
@@ -230,9 +224,10 @@ def train(model, dataset):
               5+: Train Resnet stage 5 and up
     """
     print("Training network heads")
+    total_epochs = int(math.ceil(train_subset_size / config.STEPS_PER_EPOCH))
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
-                epochs=150,
+                epochs=total_epochs,
                 layers='heads')
 
     # print("Fine tune Resnet stage 4 and up")
@@ -248,113 +243,18 @@ def train(model, dataset):
     #             layers='all')
 
 
-def color_splash(image, mask):
-    """Apply color splash effect.
-    image: RGB image [height, width, 3]
-    mask: instance segmentation mask [height, width, instance count]
-    Returns result image.
-    """
-    # Make a grayscale copy of the image. The grayscale copy still
-    # has 3 RGB channels, though.
-    gray = skimage.color.gray2rgb(skimage.color.rgb2gray(image)) * 255
-    # Copy color pixels from the original color image where mask is set
-    if mask.shape[-1] > 0:
-        # We're treating all instances as one, so collapse the mask into one layer
-        mask = (np.sum(mask, -1, keepdims=True) >= 1)
-        splash = np.where(mask, image, gray).astype(np.uint8)
-    else:
-        splash = gray.astype(np.uint8)
-    return splash
-
-
-def detect_and_color_splash(model, image_path=None, video_path=None):
-    assert image_path or video_path
-
-    # Image or video?
-    if image_path:
-        # Run model detection and generate the color splash effect
-        print("Running on {}".format(args.image))
-        # Read image
-        image = skimage.io.imread(args.image)
-        # Detect objects
-        r = model.detect([image], verbose=1)[0]
-        # Color splash
-        splash = color_splash(image, r['masks'])
-        # Save output
-        file_name = "splash_{:%Y%m%dT%H%M%S}.jpg".format(datetime.datetime.now())
-        skimage.io.imsave(file_name, splash)
-    elif video_path:
-        import cv2
-        # Video capture
-        vcapture = cv2.VideoCapture(video_path)
-        width = int(vcapture.get(cv2.CAP_PROP_FRAME_WIDTH))
-        height = int(vcapture.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        fps = vcapture.get(cv2.CAP_PROP_FPS)
-
-        # Define codec and create video writer
-        file_name = "splash_{:%Y%m%dT%H%M%S}.avi".format(datetime.datetime.now())
-        vwriter = cv2.VideoWriter(file_name,
-                                  cv2.VideoWriter_fourcc(*'MJPG'),
-                                  fps, (width, height))
-
-        count = 0
-        success = True
-        while success:
-            print("frame: ", count)
-            # Read next image
-            success, image = vcapture.read()
-            if success:
-                # OpenCV returns images as BGR, convert to RGB
-                image = image[..., ::-1]
-                # Detect objects
-                r = model.detect([image], verbose=0)[0]
-                # Color splash
-                splash = color_splash(image, r['masks'])
-                # RGB -> BGR to save image to video
-                splash = splash[..., ::-1]
-                # Add image to video writer
-                vwriter.write(splash)
-                count += 1
-        vwriter.release()
-    print("Saved to ", file_name)
-
-
 if __name__ == '__main__':
-    ############################################################
-    # command collect
-    ############################################################
-    print('For this program, we provide following functions: \n',
-          'training from scratch, training from existed weight')
-    str = input('Please enter your command:')
-
-    ############################################################
-    #  Training
-    ############################################################
     # Configurations
     config = WorkerConfig()
     config.display()
+
     # Create model
     model = modellib.MaskRCNN(mode="training", config=config, model_dir=config.DEFAULT_LOGS_DIR)
+
     # Load weights
+    model_path = initial_weight_path
+    print("Loading weights ", model_path)
+    model.load_weights(model_path, by_name=True)
 
-    weights_path = COCO_WEIGHTS_PATH
-    # Download weights file
-    if not os.path.exists(weights_path):
-        utils.download_trained_weights(weights_path)
-    model.load_weights(weights_path, by_name=True, exclude=[
-        "mrcnn_class_logits", "mrcnn_bbox_fc",
-        "mrcnn_bbox", "mrcnn_mask"])
-
-    if str == 'training from scratch':
-        dataset = input('Please enter your dataset: \n ')
-        train(model, dataset)
-    elif str == 'training from existed weight':
-        weights_path = input('Please enter the path of weights: \n ')
-        model.load_weights(weights_path, by_name=True)
-        dataset = input('Please enter your dataset: \n ')
-        train(model, dataset)
-    else:
-        print('Invalid input, exit.')
-    # *******************************************  keep train
-
-    # ******************************************* train from beginning
+    # Train
+    train(model, dataset)
